86842468582b6dad50adacef6e6c9cef3783eae6
Allison Bessett <allison_17@hotmail.com>
2017-05-12 14:20:09 -0700

    JS file

---
+++ objList.js
@@ -0,0 +1,13 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+// TODO: Create array var
+// TODO: Write function that creates a table row (tr) for each obj in array, with a cell (td) for each property to be displayed. 
+//TODO: Append each of these to the table body. 
+//TODO: Write listener for new item button. It should hide the table and show the form. It should set a listener for the form's submit button. 
+//TODO: Write event listener for submit button that creates an obj using the form data and adds that obj to the array. 
+//TODO: The listener (above) should call the first function I wrote to recreate the table with the updated database and show that table, hiding the form. 
+
+$(document).ready(function () {
+  
+});
